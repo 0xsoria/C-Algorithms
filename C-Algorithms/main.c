03f71c9ac3e490e92455568de1fc09085a1cb5e6
Gabriel Sória <gabrielss.rock@me.com>
2020-08-31 21:42:47 -0300

    Initial Commit

---
+++ C-Algorithms/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-Algorithms
+//
+//  Created by Gabriel Soria Souza on 31/08/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
